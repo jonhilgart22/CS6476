c5b3692c2632e76e4c8e7b5b8f0686fef6161eb1
Yonathan Lim <ylim@theranos.com>
2016-09-19 00:19:11 -0700

    [WIP]: ps02 4a completed

--- ps02/experiment.py
+++ ps02/experiment.py
@@ -1,5 +1,6 @@
 """Problem Set 2: Edges and Lines"""
 import os
+import math
 from ps2 import *
 
 # I/O directories
@@ -24,7 +25,19 @@ def hough_lines_draw(img_out, peaks, rho, theta):
         rho: vector of rho values, such that rho[rho_idx] is a valid rho value
         theta: vector of theta values, such that theta[theta_idx] is a valid theta value
     """
-    pass  # TODO: Your code here (nothing to return, just draw on img_out directly)
+    for peak in peaks:
+        current_rho = np.abs(rho[peak[0]])
+        current_theta = np.abs(theta[peak[1]])
+        a = np.cos(current_theta)
+        b = np.sin(current_theta)
+        x0 = a * current_rho
+        y0 = b * current_rho
+        x1 = int(x0 + 1000 * (-b))
+        y1 = int(y0 + 1000 * (a))
+        x2 = int(x0 - 1000 * (-b))
+        y2 = int(y0 - 1000 * (a))
+
+        cv2.line(img_out, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
 
 def draw_circles(img_in, circles_array):
@@ -54,69 +67,86 @@ def main():
     answers for your report. Don't submit this file.
     """
 
-    # 1-a
-    # Load the input grayscale image
-    img = cv2.imread(os.path.join(input_dir, 'ps2-input0.png'), 0)  # flags=0 ensures grayscale
-
-    # TODO: Compute edge image (img_edges). Refer to find_edges.py if you want to use a GUI to test parameters
-    img_edges = None  # TODO: Add the edge function of your choice, i.e. cv2.Canny
-    cv2.imwrite(os.path.join(output_dir, 'ps2-1-a-1.png'), img_edges)
-
-    # 2-a
-    # Compute Hough Transform for lines on edge image
-    H, rho, theta = hough_lines_acc(img_edges, rho_res=None, theta_res=None)  # TODO: Implement this
-    # TODO: Write a normalized uint8 version, mapping min value to 0 and max to 255
-    cv2.imwrite(os.path.join(output_dir, 'ps2-2-a-1.png'), H)
-
-    # 2-b
-    # Find peaks (local maxima) in accumulator array
-    peaks = hough_peaks(H, Q=None, cols=rho, rows=theta, hough_threshold=None, nhood_radii=(5, 5))  # TODO: implement this, try different parameters
-
-    highlighted_peaks = None
-    # TODO: Highlight peaks, you could use cv2.circle in each peak
-    cv2.imwrite(os.path.join(output_dir, 'ps2-2-b-1.png'), highlighted_peaks)
-
-    # 2-c
-    # Draw lines corresponding to accumulator peaks
-    img_out = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)  # copy & convert to color image
-    hough_lines_draw(img_out, peaks, rho, theta)  # TODO: implement this
-    cv2.imwrite(os.path.join(output_dir, 'ps2-2-c-1.png'), img_out)
-
-    # 3-a
-    # TODO: Read ps2-input0-noise.png, compute a smoothed image using a Gaussian filter
-    img_noise = cv2.imread(os.path.join(input_dir, 'ps2-input0-noise.png'), 0)
-    img_noise_smoothed = None  # TODO: call gaussian filter
-    cv2.imwrite(os.path.join(output_dir, 'ps2-3-a-1.png'), img_noise_smoothed)
-
-    # 3-b
-    # TODO: Compute binary edge images for both original image and smoothed version
-    img_noise_edges = None  # TODO: call edge detector function
-    img_noise_smoothed_edges = None  # TODO: call edge detector function
-    cv2.imwrite(os.path.join(output_dir, 'ps2-3-b-1.png'), img_noise_edges)
-    cv2.imwrite(os.path.join(output_dir, 'ps2-3-b-2.png'), img_noise_smoothed_edges)
-
-    # 3-c
-    # TODO: Apply Hough methods to smoothed image, tweak parameters to find best lines
-    H, rho, theta = hough_lines_acc(img_edges, rho_res=None, theta_res=None)
-    peaks = None  # TODO: call hough_peaks
-    highlighted_peaks = None
-    # TODO: Highlight peaks, you could use cv2.circle in each peak
-    cv2.imwrite(os.path.join(output_dir, 'ps2-3-c-1.png'), highlighted_peaks)
-
-    img_out_noisy = None  # TODO: draw lines on the noisy image
-    cv2.imwrite(os.path.join(output_dir, 'ps2-3-c-2.png'), img_out_noisy)
+    # # 1-a
+    # # Load the input grayscale image
+    # img = cv2.imread(os.path.join(input_dir, 'ps2-input0.png'), 0)  # flags=0 ensures grayscale
+    #
+    # # TODO: Compute edge image (img_edges). Refer to find_edges.py if you want to use a GUI to test parameters
+    filter_size = 13
+    threshold1 = 28
+    threshold2 = 115
+    # smoothed_img = cv2.GaussianBlur(img, (filter_size, filter_size), sigmaX=0, sigmaY=0)
+    # img_edges = cv2.Canny(smoothed_img, threshold1, threshold2)
+    # # img_edges = cv2.Canny(img, threshold1, threshold2)
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-1-a-1.png'), img_edges)
+    #
+    # # 2-a
+    # # Compute Hough Transform for lines on edge image
+    # H, rho, theta = hough_lines_acc(img_edges, rho_res=1, theta_res=math.pi/180)  # TODO: Implement this
+    # # TODO: Write a normalized uint8 version, mapping min value to 0 and max to 255
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-2-a-1.png'), H)
+    #
+    # # 2-b
+    # # Find peaks (local maxima) in accumulator array
+    # peaks = hough_peaks(H, Q=10, cols=rho, rows=theta, hough_threshold=200, nhood_radii=(5, 5))  # TODO: implement this, try different parameters
+    #
+    # highlighted_peaks = np.copy(H)
+    # for peak in peaks:
+    #     cv2.circle(highlighted_peaks, center=(peak[1], peak[0]), radius=20, color=255)
+    # # TODO: Highlight peaks, you could use cv2.circle in each peak
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-2-b-1.png'), highlighted_peaks)
+    #
+    # # 2-c
+    # # Draw lines corresponding to accumulator peaks
+    # img_out = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)  # copy & convert to color image
+    # hough_lines_draw(img_out, peaks, rho, theta)  # TODO: implement this
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-2-c-1.png'), img_out)
+    #
+    # # 3-a
+    # # TODO: Read ps2-input0-noise.png, compute a smoothed image using a Gaussian filter
+    # img_noise = cv2.imread(os.path.join(input_dir, 'ps2-input0-noise.png'), 0)
+    # img_noise_smoothed = cv2.GaussianBlur(img_noise, (filter_size, filter_size), sigmaX=0, sigmaY=0)  # TODO: call gaussian filter
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-3-a-1.png'), img_noise_smoothed)
+    #
+    # # 3-b
+    # # TODO: Compute binary edge images for both original image and smoothed version
+    # img_noise_edges = cv2.Canny(img_noise, threshold1, threshold2)  # TODO: call edge detector function
+    # img_noise_smoothed_edges = cv2.Canny(img_noise_smoothed, threshold1, threshold2)  # TODO: call edge detector function
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-3-b-1.png'), img_noise_edges)
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-3-b-2.png'), img_noise_smoothed_edges)
+    #
+    # # 3-c
+    # # TODO: Apply Hough methods to smoothed image, tweak parameters to find best lines
+    # H, rho, theta = hough_lines_acc(img_noise_smoothed_edges, rho_res=1, theta_res=math.pi/180)
+    # peaks = hough_peaks(H, Q=10, cols=rho, rows=theta, hough_threshold=90, nhood_radii=(5, 5))  # TODO: call hough_peaks
+    # print(peaks)
+    # highlighted_peaks = np.copy(H)
+    # for peak in peaks:
+    #     cv2.circle(highlighted_peaks, center=(peak[1], peak[0]), radius=20, color=255)
+    # # TODO: Highlight peaks, you could use cv2.circle in each peak
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-3-c-1.png'), highlighted_peaks)
+    #
+    # img_out_noisy = cv2.cvtColor(img_noise, cv2.COLOR_GRAY2BGR)  # TODO: draw lines on the noisy image
+    # hough_lines_draw(img_out_noisy, peaks, rho, theta)
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-3-c-2.png'), img_out_noisy)
 
     # 4-a
     test_circle = cv2.imread(os.path.join(input_dir, 'test_circle.png'), 0)
-    test_circle_smoothed = None  # TODO: call gaussian filter if needed
-    test_circle_edges = None  # TODO: call edge detector function using either the original or the smoothed version
+    test_circle_smoothed = cv2.GaussianBlur(test_circle, (filter_size, filter_size), sigmaX=0, sigmaY=0)  # TODO: call gaussian filter if needed
+    test_circle_edges = cv2.Canny(test_circle_smoothed, threshold1, threshold2)  # TODO: call edge detector function using either the original or the smoothed version
     H = hough_circles_acc(test_circle_edges, 75, 'single point')
+    # cv2.imwrite(os.path.join(output_dir, 'circle_H.png'), H)
     cols, rows = H.shape
-    H_peaks = hough_peaks(H, Q=None, cols=cols, rows=rows, hough_threshold=None,
+    cols = np.linspace(0, cols-1, cols)
+    rows = np.linspace(0, rows-1, rows)
+    H_peaks = hough_peaks(H, Q=1, cols=cols, rows=rows, hough_threshold=200,
                           nhood_radii=(5, 5))  # TODO: implement this, try different parameters
+    # print(H_peaks)
     cv2.imwrite(os.path.join(output_dir, 'ps2-4-a-1.png'), test_circle_edges)
 
-    highlighted_peaks = None
+    highlighted_peaks = np.copy(H)
+    for peak in H_peaks:
+        cv2.circle(highlighted_peaks, center=(peak[1], peak[0]), radius=20, color=255)
     # TODO: Highlight peaks, you could use cv2.circle in each peak
     cv2.imwrite(os.path.join(output_dir, 'ps2-4-a-2.png'), highlighted_peaks)
 
@@ -124,82 +154,82 @@ def main():
     # TODO: Modifiy hough_circles_acc to work with the 'point plus' method
     # When using 'point_plus you need to two gradient images, one in x and another in y. See cv2.Sobel
 
-    test_circle_grad_x = None  # X gradient image
-    test_circle_grad_y = None  # Y gradient image
-    H = hough_circles_acc(test_circle_edges, 75, 'point plus', grad_x=test_circle_grad_x, grad_y=test_circle_grad_y)
-    cols, rows = H.shape
-    H_peaks = hough_peaks(H, Q=None, cols=cols, rows=rows, hough_threshold=None, nhood_radii=(None, None))
-
-    # There should be only one peak given that there is one circle
-    peak_x = None  # Select the x value from H_peaks
-    peak_y = None  # Select the y value from H_peaks
-    circle = np.array([[peak_x, peak_y, 75]])
-
-    # Draw circles
-    output_image = draw_circles(test_circle, circle)
-    cv2.imwrite(os.path.join(output_dir, 'ps2-4-b-1.png'), output_image)
-
-    # From this point on you will only use the 'point plus' method
-    # 5-a
-    pens_and_coins = cv2.imread(os.path.join(input_dir, 'ps2-input1.png'), 0)
-    pens_and_coins_smoothed = None
-    pens_and_coins_edges = None
-
-    H, rho, theta = hough_lines_acc(pens_and_coins_edges, rho_res=None, theta_res=None)
-    peaks = hough_peaks(H, Q=None, cols=rho, rows=theta, hough_threshold=None, nhood_radii=(None, None))
-
-    highlighted_peaks = None
-    # TODO: Highlight peaks, you could use cv2.circle in each peak
-    cv2.imwrite(os.path.join(output_dir, 'ps2-5-a-1.png'), highlighted_peaks)
-
-    img_out = cv2.cvtColor(pens_and_coins, cv2.COLOR_GRAY2BGR)
-    hough_lines_draw(img_out, peaks, rho, theta)  # TODO: implement this
-    cv2.imwrite(os.path.join(output_dir, 'ps2-5-a-2.png'), img_out)
-
-    # 5-b
-    pens_and_coins_smoothed = None  # In case you need different parameters
-    pens_and_coins_edges = None  # In case you need different parameters
-
-    pens_and_coins_grad_x = None
-    pens_and_coins_grad_y = None
-    H = hough_circles_acc(pens_and_coins_edges, 20, 'point plus',
-                          grad_x=pens_and_coins_grad_x, grad_y=pens_and_coins_grad_y)
-    cols, rows = H.shape
-    H_peaks = hough_peaks(H, Q=None, cols=cols, rows=rows, hough_threshold=None, nhood_radii=(None, None))
-
-    # There could be more than one peak returned. Now draw all the non-overlapping circles
-    peaks_x = [None]  # Peaks x values
-    peaks_y = [None]  # Peaks y values
-    peaks_r = [20] * len(peaks_x)
-    circles = np.column_stack((peaks_x, peaks_y, peaks_r))
-
-    # Draw circles
-    output_image = draw_circles(pens_and_coins, circles)
-    cv2.imwrite(os.path.join(output_dir, 'ps2-5-b-1.png'), output_image)
-
-    # 5-c
-    # find_circles should call hough_circles_acc and hough_peaks.
-    pens_and_coins_smoothed = None  # In case you need different parameters
-    pens_and_coins_edges = None  # In case you need different parameters
-
-    pens_and_coins_grad_x = None  # X gradient image
-    pens_and_coins_grad_y = None  # Y gradient image
-    radii_range = range(20, 50, 5)  # Try different values
-    circles = find_circles(pens_and_coins_edges, pens_and_coins_grad_x, pens_and_coins_grad_y,
-                           radii=radii_range, hough_threshold=None, nhood_radii=None)
-
-    output_image = draw_circles(pens_and_coins, circles)
-    cv2.imwrite(os.path.join(output_dir, 'ps2-5-c-1.png'), output_image)
-
-    # 6
-    cluttered_image = cv2.imread(os.path.join(input_dir, 'ps2-input2.png'), 0)
-    # TODO: Identify the lines and circles in the cluttered image
-    # TODO: save image ps2-6-a-1.png
-    # TODO: save image ps2-6-b-1.png
-    # TODO: save image ps2-6-c-1.png
-
-    # TODO: Don't forget to answer questions 7 and 8 in your report
-    pass
+    test_circle_grad_x = cv2.Sobel(test_circle, cv2.CV_8U, 1, 0, ksize=3)  # X gradient image
+    test_circle_grad_y = cv2.Sobel(test_circle, cv2.CV_8U, 0, 1, ksize=3)  # Y gradient image
+    # H = hough_circles_acc(test_circle_edges, 75, 'point plus', grad_x=test_circle_grad_x, grad_y=test_circle_grad_y)
+    # cols, rows = H.shape
+    # H_peaks = hough_peaks(H, Q=None, cols=cols, rows=rows, hough_threshold=None, nhood_radii=(None, None))
+    #
+    # # There should be only one peak given that there is one circle
+    # peak_x = None  # Select the x value from H_peaks
+    # peak_y = None  # Select the y value from H_peaks
+    # circle = np.array([[peak_x, peak_y, 75]])
+    #
+    # # Draw circles
+    # output_image = draw_circles(test_circle, circle)
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-4-b-1.png'), output_image)
+    #
+    # # From this point on you will only use the 'point plus' method
+    # # 5-a
+    # pens_and_coins = cv2.imread(os.path.join(input_dir, 'ps2-input1.png'), 0)
+    # pens_and_coins_smoothed = None
+    # pens_and_coins_edges = None
+    #
+    # H, rho, theta = hough_lines_acc(pens_and_coins_edges, rho_res=None, theta_res=None)
+    # peaks = hough_peaks(H, Q=None, cols=rho, rows=theta, hough_threshold=None, nhood_radii=(None, None))
+    #
+    # highlighted_peaks = None
+    # # TODO: Highlight peaks, you could use cv2.circle in each peak
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-5-a-1.png'), highlighted_peaks)
+    #
+    # img_out = cv2.cvtColor(pens_and_coins, cv2.COLOR_GRAY2BGR)
+    # hough_lines_draw(img_out, peaks, rho, theta)  # TODO: implement this
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-5-a-2.png'), img_out)
+    #
+    # # 5-b
+    # pens_and_coins_smoothed = None  # In case you need different parameters
+    # pens_and_coins_edges = None  # In case you need different parameters
+    #
+    # pens_and_coins_grad_x = None
+    # pens_and_coins_grad_y = None
+    # H = hough_circles_acc(pens_and_coins_edges, 20, 'point plus',
+    #                       grad_x=pens_and_coins_grad_x, grad_y=pens_and_coins_grad_y)
+    # cols, rows = H.shape
+    # H_peaks = hough_peaks(H, Q=None, cols=cols, rows=rows, hough_threshold=None, nhood_radii=(None, None))
+    #
+    # # There could be more than one peak returned. Now draw all the non-overlapping circles
+    # peaks_x = [None]  # Peaks x values
+    # peaks_y = [None]  # Peaks y values
+    # peaks_r = [20] * len(peaks_x)
+    # circles = np.column_stack((peaks_x, peaks_y, peaks_r))
+    #
+    # # Draw circles
+    # output_image = draw_circles(pens_and_coins, circles)
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-5-b-1.png'), output_image)
+    #
+    # # 5-c
+    # # find_circles should call hough_circles_acc and hough_peaks.
+    # pens_and_coins_smoothed = None  # In case you need different parameters
+    # pens_and_coins_edges = None  # In case you need different parameters
+    #
+    # pens_and_coins_grad_x = None  # X gradient image
+    # pens_and_coins_grad_y = None  # Y gradient image
+    # radii_range = range(20, 50, 5)  # Try different values
+    # circles = find_circles(pens_and_coins_edges, pens_and_coins_grad_x, pens_and_coins_grad_y,
+    #                        radii=radii_range, hough_threshold=None, nhood_radii=None)
+    #
+    # output_image = draw_circles(pens_and_coins, circles)
+    # cv2.imwrite(os.path.join(output_dir, 'ps2-5-c-1.png'), output_image)
+    #
+    # # 6
+    # cluttered_image = cv2.imread(os.path.join(input_dir, 'ps2-input2.png'), 0)
+    # # TODO: Identify the lines and circles in the cluttered image
+    # # TODO: save image ps2-6-a-1.png
+    # # TODO: save image ps2-6-b-1.png
+    # # TODO: save image ps2-6-c-1.png
+    #
+    # # TODO: Don't forget to answer questions 7 and 8 in your report
+    # pass
 
 
 if __name__ == '__main__':
--- ps02/ps2.py
+++ ps02/ps2.py
@@ -3,9 +3,10 @@
 import math
 import numpy as np
 import cv2
+import time
 
 
-def hough_lines_acc(img_edges, rho_res=1, theta_res=None):
+def hough_lines_acc(img_edges, rho_res=1, theta_res=np.pi/180):
     """Compute Hough Transform for lines on edge image.
 
     Parameters
@@ -20,8 +21,26 @@ def hough_lines_acc(img_edges, rho_res=1, theta_res=None):
         rho (numpy.array): vector of rho values, one for each row of H
         theta (numpy.array): vector of theta values in the range [0,pi), one for each column of H
     """
+    rows, cols = img_edges.shape
+    # construct theta array [0, pi)
+    theta = np.array([i * theta_res for i in xrange(0, 180)])
+    # get the maximum rho possible
+    rho_max = int(math.ceil(math.sqrt(rows**2 + cols**2)))
+    # construct the rho array
+    rho = np.array([i * rho_res for i in xrange(-rho_max, rho_max)])
+    # initialize H[d, theta] = 0
+    H = np.zeros((rho.size, theta.size))
 
-    pass  # TODO: change to return H, rho, theta
+    for y in range(rows):
+        for x in range(cols):
+            # for each edge point
+            if img_edges[y, x] == 255:
+                for i, t in enumerate(theta):
+                    d = x * math.cos(t) - y * math.sin(t)  # maybe negative
+                    rho_index = np.argmin(np.abs(rho - d))
+                    H[rho_index, i] += 1
+
+    return H, rho, theta
 
 
 def hough_peaks(H, Q, cols, rows, hough_threshold=200, nhood_radii=(5, 5)):
@@ -42,8 +61,41 @@ def hough_peaks(H, Q, cols, rows, hough_threshold=200, nhood_radii=(5, 5)):
         peaks (numpy.array): A matrix where each row is a (col_id, row_id) pair where the peaks are.
 
     """
+    # get the possible peaks index given the threshold
+    p_rows, p_cols = np.where(H >= hough_threshold)
+    # get the peak values
+    values = H[p_rows, p_cols]
+    # arrange matrix in the order of [[x, y, value], [x, y, value]]
+    possible_peaks = np.transpose(np.concatenate((p_rows, p_cols, values)).reshape(3, values.size))
+    # sort descending order by value of the most voted peak
+    possible_peaks = possible_peaks[possible_peaks[:, 2].argsort()[::-1]]
 
-    pass  # TODO: change to return peaks
+    # create a clone copy of H
+    newH = np.copy(H)
+    # get the padding to the left and right of the peak index
+    left_padding = int(math.floor(nhood_radii[0] / 2.))
+    right_padding = int(math.ceil(nhood_radii[1] / 2.))
+    new_possible_peaks = []
+
+    # update the newH so that the values around a peak is 0
+    for peak in possible_peaks:
+        if newH[peak[0], peak[1]] == peak[2]:
+            x0 = peak[0] - left_padding if peak[0] >= left_padding else 0
+            x1 = peak[0] + right_padding if peak[0] <= (cols.size - right_padding) else cols.size
+            y0 = peak[1] - left_padding if peak[1] >= left_padding else 0
+            y1 = peak[1] + right_padding if peak[1] <= (rows.size - right_padding) else rows.size
+            # set values around the peak to 0
+            newH[x0:x1, y0:y1] = 0
+            # restore the peak value
+            newH[peak[0], peak[1]] = peak[2]
+            # only interested in the peaks that are not replaced
+            new_possible_peaks.append(peak)
+
+    possible_peaks = np.array(new_possible_peaks).astype(np.int)
+
+    # only return Q pairs
+    peaks = possible_peaks[:, :2][:Q]
+    return peaks
 
 
 def hough_circles_acc(img_edges, radius=10, method='point plus', grad_x=None, grad_y=None):
@@ -61,8 +113,49 @@ def hough_circles_acc(img_edges, radius=10, method='point plus', grad_x=None, gr
     -------
         H (numpy.array): Hough accumulator array
     """
+    rows, cols = img_edges.shape
+    # construct theta array [0, 2*pi)
+    theta_res = np.pi/180
+    theta = np.array([i * theta_res for i in xrange(0, 360)])
+    # # get the maximum rho possible
+    # rho_max = int(math.ceil(math.sqrt(rows**2 + cols**2)))
+    # # construct the rho array
+    # rho = np.array([i * rho_res for i in xrange(-rho_max, rho_max)])
+    # initialize H[d, theta] = 0
+    H = np.zeros((rows, cols))
 
-    pass  # TODO: change to return H
+    if method == 'single point':
+        # (x-a)**2 + (y-b)**2 = r**2
+
+        # for y in range(rows):
+        #     for x in range(cols):
+        #         # for each edge point
+        #         if img_edges[y, x] == 255:
+        #             for i, t in enumerate(theta):
+        #                 d = x * math.cos(t) - y * math.sin(t)  # maybe negative
+        #                 rho_index = np.argmin(np.abs(rho - d))
+        #                 H[rho_index, i] += 1
+
+        # For every edge pixel (x, y):
+        #     For every t:
+        #         a = x - r * cos(t)
+        #         b = y + r * sin(t)
+        #         H[a, b] += 1
+        #     End
+        # End
+        for y in range(rows):
+            for x in range(cols):
+                # for each edge point
+                if img_edges[y, x] == 255:
+                    for i, t in enumerate(theta):
+                        a = x - radius * math.cos(t)
+                        b = y + radius * math.sin(t)
+                        if 0 <= a < rows and 0 <= b < cols:
+                            H[a, b] += 1
+    elif method == 'point plus':
+        pass
+
+    return H
 
 
 def find_circles(edge_img, grad_x, grad_y, radii=None, hough_threshold=None, nhood_radii=None):
