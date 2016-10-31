bc6193239d0ff07c83fa4a1eddd2f072f68883dc
Yonathan Lim <ylim@theranos.com>
2016-10-31 02:53:39 -0700

    ps05: autograder passed

--- ps05/experiment.py
+++ ps05/experiment.py
@@ -27,6 +27,44 @@ def main():
     answers for your report.
     """
 
+    # check = cv2.imread(os.path.join(input_dir, 'test_out1.png'), 0) / 255.
+    # check_x = gradient_x(check)
+    # check_y = gradient_y(check)
+    # check_pair = make_image_pair(check_x, check_y)
+    # check_pair = cv2.normalize(check_pair, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    # cv2.imwrite(os.path.join(output_dir, "check_pair.png"), check_pair)
+    #
+    # kernel_dims = (3, 3)
+    # alpha = 0.06
+    # check_r = harris_response(check_x, check_y, kernel_dims, alpha)
+    # check_r = cv2.normalize(check_r, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    # cv2.imwrite(os.path.join(output_dir, "check_r.png"), check_r)
+    #
+    # threshold = 0.6
+    # radius = 3
+    # check_corners = find_corners(check_r, threshold, radius)
+    # check_out = draw_corners(check, check_corners)
+    # check_out = cv2.normalize(check_out, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    # cv2.imwrite(os.path.join(output_dir, "check_out.png"), check_out)
+    #
+    # check_rot = cv2.imread(os.path.join(input_dir, 'test_out2.png'), 0) / 255.
+    # check_rot_x = gradient_x(check_rot)
+    # check_rot_y = gradient_y(check_rot)
+    # check_rot_pair = make_image_pair(check_rot_x, check_rot_y)
+    # check_rot_pair = cv2.normalize(check_rot_pair, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    # cv2.imwrite(os.path.join(output_dir, "check_rot_pair.png"), check_rot_pair)
+    #
+    # check_rot_r = harris_response(check_rot_x, check_rot_y, kernel_dims, alpha)
+    # check_rot_r = cv2.normalize(check_rot_r, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    # cv2.imwrite(os.path.join(output_dir, "check_rot_r.png"), check_rot_r)
+    #
+    # check_rot_corners = find_corners(check_rot_r, threshold, radius)
+    # check_rot_out = draw_corners(check_rot, check_rot_corners)
+    # check_rot_out = cv2.normalize(check_rot_out, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    # cv2.imwrite(os.path.join(output_dir, "check_rot_out.png"), check_rot_out)
+
+
+
     # 1a
     trans_a = cv2.imread(os.path.join(input_dir, "transA.jpg"), 0) / 255.
     trans_a_x = gradient_x(trans_a)  # TODO: implement this
@@ -49,8 +87,8 @@ def main():
     sim_b = cv2.imread(os.path.join(input_dir, "simB.jpg"), 0) / 255.
 
     # 1b
-    kernel_dims = None  # TODO: Define a the kernel dimensions (tuple)
-    alpha = None  # TODO: Set a float value for alpha
+    kernel_dims = (3, 3)  # TODO: Define a the kernel dimensions (tuple)
+    alpha = 0.06  # TODO: Set a float value for alpha
     trans_a_r = harris_response(trans_a_x, trans_a_y, kernel_dims, alpha)  # TODO: implement this
     trans_a_r = cv2.normalize(trans_a_r, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
     cv2.imwrite(os.path.join(output_dir, "ps5-1-b-1.png"), trans_a_r)
@@ -72,8 +110,8 @@ def main():
     cv2.imwrite(os.path.join(output_dir, "ps5-1-b-4.png"), sim_b_r)
 
     # 1c
-    threshold = None  # TODO: define a threshold value
-    radius = None  # TODO: define a radius value
+    threshold = 0.5  # TODO: define a threshold value
+    radius = 5  # TODO: define a radius value
     trans_a_corners = find_corners(trans_a_r, threshold, radius)  # TODO: implement this
     trans_a_out = draw_corners(trans_a, trans_a_corners)  # TODO: implement this
     trans_a_out = cv2.normalize(trans_a_out, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
@@ -89,7 +127,7 @@ def main():
                                           output_filename="ps5-1-c-4.png")
 
     # 2a
-    size = None  # TODO: Define a size value
+    size = 10  # TODO: Define a size value
     octave = 0  # You can leave this value to 0
     trans_a_angle = gradient_angle(trans_a_x, trans_a_y)  # TODO: implement this
     trans_a_kp, trans_a_out = get_draw_keypoints(trans_a_corners, trans_a_r, trans_a_angle, size, octave)   # TODO: implement this
@@ -123,31 +161,35 @@ def main():
     cv2.imwrite(os.path.join(output_dir, "ps5-2-b-2.png"), sim_a_b_out)
 
     # 3a
-    threshold = None  # TODO: define a threshold value
+    threshold = 15  # TODO: define a threshold value
     translation, good_matches = compute_translation_RANSAC(trans_a_kp, trans_b_kp, trans_matches,
                                                            threshold)  # TODO: implement this
-    trans_pair = make_image_pair(trans_a, trans_b)
+    # trans_pair = make_image_pair(trans_a, trans_b)
     print '3a: Translation vector: \n', translation
 
     # TODO: Draw biggest consensus set lines on trans_pair. Try to use different colors for each line.
-    # cv2.imwrite(os.path.join(output_dir, "ps5-3-a-1.png"), trans_pair)
+    trans_pair = draw_matches(trans_a, trans_b, trans_a_kp, trans_b_kp, good_matches)
+    cv2.imwrite(os.path.join(output_dir, "ps5-3-a-1.png"), trans_pair)
 
     # 3b
-    threshold = None  # TODO: define a threshold value
+    threshold = 20  # TODO: define a threshold value
     similarity, sim_good_matches = compute_similarity_RANSAC(sim_a_kp, sim_b_kp, sim_matches,
                                                              threshold)  # TODO: implement this
     print '3b: Transform Matrix for the best set: \n', similarity
 
     # TODO: Draw biggest consensus set lines on trans_pair. Try to use different colors for each line.
-    # cv2.imwrite(os.path.join(output_dir, "ps5-3-b-1.png"), sim_pair)
+    sim_pair = draw_matches(sim_a, sim_b, sim_a_kp, sim_b_kp, sim_good_matches)
+    cv2.imwrite(os.path.join(output_dir, "ps5-3-b-1.png"), sim_pair)
 
     # 3c
-    threshold = None  # TODO: define a threshold value
+    threshold = 20  # TODO: define a threshold value
     similarity_affine, sim_good_matches = compute_affine_RANSAC(sim_a_kp, sim_b_kp, sim_matches,
                                                                 threshold)  # TODO: implement this
     print '3c: Transform Matrix for the best set: \n', similarity_affine
+
     # TODO: Draw biggest consensus set lines on trans_pair. Try to use different colors for each line.
-    # cv2.imwrite(os.path.join(output_dir, "ps5-3-c-1.png"), sim_pair)
+    sim_pair = draw_matches(sim_a, sim_b, sim_a_kp, sim_b_kp, sim_good_matches)
+    cv2.imwrite(os.path.join(output_dir, "ps5-3-c-1.png"), sim_pair)
 
     # 3d
     warpedB, overlay = warp_img(sim_a, sim_b, similarity)
--- ps05/ps5.py
+++ ps05/ps5.py
@@ -1,5 +1,6 @@
 """Problem Set 5: Harris, ORB, RANSAC."""
 
+import scipy.stats as st
 import numpy as np
 import cv2
 
@@ -15,9 +16,8 @@ def gradient_x(image):
     -------
         Ix (numpy.array): image gradient in X direction, values in [-1.0, 1.0]
     """
-
-    # TODO: Your code here
-    pass  # TODO: Change to return Ix
+    gradient = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+    return gradient / np.linalg.norm(gradient)
 
 
 def gradient_y(image):
@@ -31,9 +31,8 @@ def gradient_y(image):
     -------
         Iy (numpy.array): image gradient in Y direction, values in [-1.0, 1.0]
     """
-
-    # TODO: Your code here
-    pass  # TODO: Change to return Iy
+    gradient = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+    return gradient / np.linalg.norm(gradient)
 
 
 def make_image_pair(image1, image2):
@@ -52,9 +51,21 @@ def make_image_pair(image1, image2):
     -------
         image_pair (numpy.array): combination of both images, side-by-side, same type.
     """
-
-    # TODO: Your code here
-    pass  # TODO: Change to return image_pair
+    img1_shape = image1.shape
+    img2_shape = image2.shape
+    if img1_shape[0] == img2_shape[0]:
+        img1 = image1
+        img2 = image2
+    elif img1_shape[0] > img2_shape[0]:
+        img1 = image1
+        pad_tuple = tuple([(0, img1_shape[0] - img2_shape[0])] + [(0, 0) for _ in xrange(len(img1_shape) - 1)])
+        img2 = np.pad(image2, pad_tuple, 'constant', constant_values=0)
+    else:
+        img2 = image2
+        pad_tuple = tuple([(0, img2_shape[0] - img1_shape[0])] + [(0, 0) for _ in xrange(len(img2_shape) - 1)])
+        img1 = np.pad(image1, pad_tuple, 'constant', constant_values=0)
+    # TODO: handle combination of grayscale and color images
+    return np.concatenate((img1, img2), axis=1)
 
 
 def harris_response(Ix, Iy, kernel, alpha):
@@ -71,9 +82,33 @@ def harris_response(Ix, Iy, kernel, alpha):
     -------
         R (numpy.array): Harris response map, same size as inputs, floating-point
     """
+    # uniform kernel
+    w = np.ones(kernel) / (kernel[0] * kernel[1])
 
-    # TODO: Your code here
-    pass  # TODO: Change to return R
+    # # gaussian kernel
+    # kx, ky = kernel
+    # sigma = 3  # np.max([kx, ky])
+    # x_interval = (2 * sigma + 1.) / kx
+    # y_interval = (2 * sigma + 1.) / ky
+    # x_space = np.linspace(-sigma - x_interval/2., sigma + x_interval/2., kx+1)
+    # y_space = np.linspace(-sigma - y_interval/2., sigma + y_interval/2., ky+1)
+    # # x_space = np.linspace(-kx, kx, kx+1)
+    # # y_space = np.linspace(-ky, ky, ky+1)
+    # x_diff = np.diff(st.norm.cdf(x_space))
+    # y_diff = np.diff(st.norm.cdf(y_space))
+    # kernel_raw = np.sqrt(np.outer(x_diff, y_diff))
+    # w = kernel_raw / kernel_raw.sum()
+    # w_out = cv2.normalize(w, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+    M00 = cv2.filter2D(Ix * Ix, -1, w)
+    M01 = M10 = cv2.filter2D(Ix * Iy, -1, w)
+    M11 = cv2.filter2D(Iy * Iy, -1, w)
+
+    det_M = (M00 * M11) - (M01 * M10)
+    trace_M = M00 + M11
+
+    R = det_M - (alpha * (trace_M ** 2))
+    return R
 
 
 def find_corners(R, threshold, radius):
@@ -97,8 +132,33 @@ def find_corners(R, threshold, radius):
     R_norm = cv2.normalize(R, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
     # # Do not modify the code above. Continue working with R_norm. # #
 
-    # TODO: Your code here
-    pass  # TODO: Change to return corners
+    if threshold < 0. or threshold > 1.:
+        return np.array([])
+
+    # get the possible peaks indices given the threshold
+    p_rows, p_cols = np.where(R_norm >= threshold)
+    # get the peak values
+    values = R_norm[p_rows, p_cols]
+    # arrange matrix in the order of [[y, x, value], [y, x, value]]
+    possible_peaks = np.vstack((p_rows, p_cols, values)).T
+    # sort descending order by value of the most bright
+    possible_peaks = possible_peaks[possible_peaks[:, 2].argsort()[::-1]]
+
+    new_R_norm = R_norm.copy()
+    peaks = []
+
+    for peak in possible_peaks:
+        if new_R_norm[peak[0], peak[1]] == peak[2]:
+            y0 = peak[0] - radius - 1 if peak[0] >= radius else 0
+            y1 = peak[0] + radius + 2 if peak[0] <= R.shape[0] - radius else R.shape[0]
+            x0 = peak[1] - radius - 1 if peak[1] >= radius else 0
+            x1 = peak[1] + radius + 2 if peak[1] <= R.shape[1] - radius else R.shape[1]
+            new_R_norm[y0:y1, x0:x1] = 0
+            new_R_norm[peak[0], peak[1]] = peak[2]
+            peaks.append(peak)
+
+    peaks = np.array(peaks)[:, :2][:, ::-1]
+    return peaks.astype(np.int)
 
 
 def draw_corners(image, corners):
@@ -114,9 +174,13 @@ def draw_corners(image, corners):
     -------
         image_out (numpy.array): copy of image with corners drawn on it, in color (BGR).
     """
+    image_norm = cv2.normalize(image, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
-    # TODO: Your code here
-    pass  # TODO: Change to return image_out
+    image_out = cv2.cvtColor(image_norm, cv2.COLOR_GRAY2BGR)
+
+    for corner in corners:
+        cv2.circle(image_out, tuple(corner), 2, (0, 255, 0), -1)
+    return image_out
 
 
 def gradient_angle(Ix, Iy):
@@ -131,9 +195,8 @@ def gradient_angle(Ix, Iy):
     -------
         angle (numpy.array): gradient angle image, same shape as Ix and Iy. Values must be in degrees [0.0, 360)
     """
-
-    # TODO: Your code here
-    pass  # TODO: Change to return angle
+    angle = np.arctan2(Iy, Ix) / np.pi * 180
+    return angle
 
 
 def get_draw_keypoints(points, R, angle, size, octave=0):
@@ -156,10 +219,17 @@ def get_draw_keypoints(points, R, angle, size, octave=0):
         keypoints (list): a sequence of cv2.KeyPoint objects
         image_out (numpy.array): output image with keypoints drawn on it
     """
+    keypoints = []
+    for point in points:
+        x, y = point
+        keypoint = cv2.KeyPoint(x=x, y=y, _size=size, _angle=angle[y, x], _octave=octave)
+        keypoints.append(keypoint)
 
-    # TODO: Your code here
-    # Note: You should be able to plot the keypoints using cv2.drawKeypoints() in OpenCV 2.4.9+
-    pass  # TODO: Change to return keypoints, image_out
+    R_norm = cv2.normalize(R, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    image_out = cv2.cvtColor(R_norm, cv2.COLOR_GRAY2BGR)
+    image_out = cv2.drawKeypoints(image_out, keypoints)
+
+    return keypoints, image_out
 
 
 def get_descriptors(image, keypoints):
@@ -178,9 +248,9 @@ def get_descriptors(image, keypoints):
     image_norm = np.zeros(image.shape, dtype=np.uint8)
     cv2.normalize(image, dst=image_norm, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 
-    # TODO: Your code here. Work with image_norm.
-    # Note: You can use OpenCV's ORB.compute() method to extract descriptors, or write your own!
-    pass  # TODO: Change to return descriptors, new_kp
+    orb = cv2.ORB()
+    keypoints, descriptors = orb.compute(image_norm, keypoints)
+    return descriptors, keypoints
 
 
 def match_descriptors(desc1, desc2):
@@ -197,11 +267,9 @@ def match_descriptors(desc1, desc2):
     -------
         matches (list): a sequence (list) of cv2.DMatch objects containing corresponding descriptor indices
     """
-
-    # TODO: Your code here
-    # Note: You can use OpenCV's descriptor matchers, or write your own!
-    #       Make sure you use Hamming Normalization to match the autograder.
-    pass  # TODO: Change to return matches
+    bfm = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bfm.match(desc1, desc2)
+    return matches
 
 
 def draw_matches(image1, image2, kp1, kp2, matches):
@@ -223,9 +291,24 @@ def draw_matches(image1, image2, kp1, kp2, matches):
                                  color image (BGR), uint8, values in [0, 255]
     """
 
-    # TODO: Your code here
-    # Note: DO NOT use OpenCV's match drawing function(s)! Write your own.
-    pass  # TODO: Change to return image_out
+    image1_norm = cv2.normalize(image1, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    image2_norm = cv2.normalize(image2, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    image_out = make_image_pair(image1_norm, image2_norm)
+    image_out = cv2.cvtColor(image_out, cv2.COLOR_GRAY2BGR)
+
+    for match in matches:
+        kp1_idx = match.queryIdx
+        kp2_idx = match.trainIdx
+        x1, y1 = np.array(kp1[kp1_idx].pt, dtype=np.int)
+        x2, y2 = np.array(kp2[kp2_idx].pt, dtype=np.int)
+
+        cv2.line(image_out, (x1, y1), (x2 + image1.shape[1], y2), color=(0, 255, 0))
+
+    return image_out
+
+
+def calculate_ssd(actual, prediction):
+    return np.sqrt(np.sum(np.square(prediction - actual)) / len(actual))
 
 
 def compute_translation_RANSAC(kp1, kp2, matches, thresh=0):
@@ -243,9 +326,52 @@ def compute_translation_RANSAC(kp1, kp2, matches, thresh=0):
         translation (numpy.array): translation/offset vector <x, y>, array of shape (2, 1)
         good_matches (list): consensus set of matches that agree with this translation
     """
+    translation = np.zeros((2, 1))
+    good_matches = []
+    s = 1
 
-    # TODO: Your code here
-    pass  # TODO: Change to return translation, good_matches
+    if len(matches) < s:
+        return translation, good_matches
+
+    N = np.inf
+    sample_count = 0
+    e = 1.
+    p = .99
+
+    while N > sample_count:
+        sample = np.random.choice(matches, s, replace=False)[0]
+        t1 = np.array(kp1[sample.queryIdx].pt)
+        t2 = np.array(kp2[sample.trainIdx].pt)
+        _translation = t2 - t1
+
+        inliers = []
+        t_avg = np.zeros((0, 2))
+        for match in matches:
+            p1 = np.array(kp1[match.queryIdx].pt)
+            p2 = np.array(kp2[match.trainIdx].pt)
+            d = p2 - p1
+
+            # difference between d and _translation
+            ssd = calculate_ssd(d, _translation)
+            if ssd <= thresh:
+                inliers.append(match)
+                t_avg = np.concatenate((t_avg, [d]))
+
+        if len(good_matches) < len(inliers) or len(good_matches) == 0:
+            good_matches = inliers
+            translation = np.average(t_avg, axis=0).reshape(2, 1)
+
+        e0 = 1 - (len(inliers) / float(len(matches)))
+
+        if e0 < e:
+            e = e0
+            N = np.log(1 - p) / np.log(1 - np.power(1 - e, s))
+
+        sample_count += 1
+
+    print len(good_matches) / float(len(matches))
+
+    return translation, good_matches
 
 
 def compute_similarity_RANSAC(kp1, kp2, matches, thresh=0):
@@ -263,9 +389,65 @@ def compute_similarity_RANSAC(kp1, kp2, matches, thresh=0):
         transform (numpy.array): similarity transform matrix, NumPy array of shape (2, 3)
         good_matches (list): consensus set of matches that agree with this transform
     """
+    transform = np.zeros((2, 3))
+    good_matches = []
+    s = 2
 
-    # TODO: Your code here
-    pass  # TODO: Change to return transform, good_matches
+    if len(matches) < s:
+        return transform, good_matches
+
+    N = np.inf
+    sample_count = 0
+    e = 1.
+    p = .99
+
+    def calculate_transform(kp1, kp2, samples):
+        A = np.zeros((0, 4))
+        B = np.zeros((0, 1))
+        for sample in samples:
+            u, v = np.array(kp1[sample.queryIdx].pt)
+            u_p, v_p = np.array(kp2[sample.trainIdx].pt)
+            A = np.concatenate((A, [[u, -v, 1, 0],
+                                    [v,  u, 0, 1]]))
+            B = np.concatenate((B, [[u_p],
+                                    [v_p]]))
+        a, b, c, d = np.linalg.lstsq(A, B)[0]
+        transform = np.array([[a, -b, c],
+                              [b,  a, d]]).reshape(2, 3)
+        return transform
+
+    while N > sample_count:
+        samples = np.random.choice(matches, s, replace=False)
+        _transform = calculate_transform(kp1, kp2, samples)
+
+        inliers = []
+        for match in matches:
+            p1 = np.array(kp1[match.queryIdx].pt)
+            p2 = np.array(kp2[match.trainIdx].pt)
+            h_p1 = np.concatenate((p1, [1])).reshape(3, 1)
+            ep = np.dot(_transform, h_p1)
+
+            # difference between p2 and ep
+            ssd = calculate_ssd(p2, ep.reshape(ep.shape[0],))
+            if ssd <= thresh:
+                inliers.append(match)
+
+        if len(good_matches) < len(inliers) or len(good_matches) == 0:
+            good_matches = inliers
+
+        e0 = 1 - (len(inliers) / float(len(matches)))
+
+        if e0 < e:
+            e = e0
+            N = np.log(1 - p) / np.log(1 - np.power(1 - e, s))
+
+        sample_count += 1
+
+    transform = calculate_transform(kp1, kp2, good_matches)
+
+    print len(good_matches) / float(len(matches))
+
+    return transform, good_matches
 
 
 def compute_affine_RANSAC(kp1, kp2, matches, thresh=0):
@@ -283,9 +465,65 @@ def compute_affine_RANSAC(kp1, kp2, matches, thresh=0):
         transform (numpy.array): affine transform matrix, NumPy array of shape (2, 3)
         good_matches (list): consensus set of matches that agree with this transform
     """
+    transform = np.zeros((2, 3))
+    good_matches = []
+    s = 3
 
-    # TODO: Your code here
-    pass  # TODO: Change to return transform, good_matches
+    if len(matches) < s:
+        return transform, good_matches
+
+    N = np.inf
+    sample_count = 0
+    e = 1.
+    p = .99
+
+    def calculate_transform(kp1, kp2, samples):
+        A = np.zeros((0, 6))
+        B = np.zeros((0, 1))
+        for sample in samples:
+            u, v = np.array(kp1[sample.queryIdx].pt)
+            u_p, v_p = np.array(kp2[sample.trainIdx].pt)
+            A = np.concatenate((A, [[u, v, 1, 0, 0, 0],
+                                    [0, 0, 0, u, v, 1]]))
+            B = np.concatenate((B, [[u_p],
+                                    [v_p]]))
+        a, b, c, d, e, f = np.linalg.lstsq(A, B)[0]
+        transform = np.array([[a, b, c],
+                              [d, e, f]]).reshape(2, 3)
+        return transform
+
+    while N > sample_count:
+        samples = np.random.choice(matches, s, replace=False)
+        _transform = calculate_transform(kp1, kp2, samples)
+
+        inliers = []
+        for match in matches:
+            p1 = np.array(kp1[match.queryIdx].pt)
+            p2 = np.array(kp2[match.trainIdx].pt)
+            h_p1 = np.concatenate((p1, [1])).reshape(3, 1)
+            ep = np.dot(_transform, h_p1)
+
+            # difference between p2 and ep
+            ssd = calculate_ssd(p2, ep.reshape(ep.shape[0],))
+            if ssd <= thresh:
+                inliers.append(match)
+
+        if len(good_matches) < len(inliers) or len(good_matches) == 0:
+            good_matches = inliers
+
+        e0 = 1 - (len(inliers) / float(len(matches)))
+
+        if e0 < e:
+            e = e0
+            N = np.log(1 - p) / np.log(1 - np.power(1 - e, s))
+
+        sample_count += 1
+
+    transform = calculate_transform(kp1, kp2, good_matches)
+
+    print len(good_matches) / float(len(matches))
+
+    return transform, good_matches
 
 
 def warp_img(img_a, img_b, transform):
@@ -312,5 +550,29 @@ def warp_img(img_a, img_b, transform):
                                green channel
     """
 
-    # TODO: Your code here
-    pass  # TODO: Change to return overlay, warpedB
+    warpedB = np.zeros_like(img_b)
+    for x in xrange(img_b.shape[1]):
+        for y in xrange(img_b.shape[0]):
+            h_p = np.array([[x], [y], [1]])
+            i, j = np.dot(transform, h_p).reshape(2,)
+            if j < 0 or j >= img_b.shape[0] - 1:
+                warpedB[y, x] = 0.
+            elif i < 0 or i >= img_b.shape[1] - 1:
+                warpedB[y, x] = 0.
+            else:
+                a = i - np.floor(i)
+                b = j - np.floor(j)
+                # bilinear interpolation
+                warpedB[y, x] = (1 - a) * (1 - b) * img_b[j, i]
+                warpedB[y, x] += a * (1 - b) * img_b[j, i + 1]
+                warpedB[y, x] += a * b * img_b[j + 1, i + 1]
+                warpedB[y, x] += (1 - a) * b * img_b[j + 1, i]
+
+    warpedB = cv2.normalize(warpedB, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+    img_a_norm = cv2.normalize(img_a, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+
+    overlay = np.zeros(list(img_a.shape) + [3])
+    overlay[..., 2] = img_a_norm  # red channel
+    overlay[..., 1] = warpedB  # green channel
+
+    return warpedB, overlay
